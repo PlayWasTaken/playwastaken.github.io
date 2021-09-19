0bcd9cb7ed75b02891c0f081bfe181a240a4934d
BearWasTaken <84278620+BearWasTakenMC@users.noreply.github.com>
2021-09-19 10:59:29 +0800

    Update lmfao.js

--- lmfao.js
+++ lmfao.js
@@ -1,3 +1,8 @@
+newWin = window.open(window.location.href);
+if(!newWin || newWin.closed || typeof newWin.closed=='undefined') { 
+  alert("Please enable popups inorder for the ads to work");
+}
+
 alert("Still using this device?");
 setInterval(() => {
   window.open(window.location.href);
